d7188e2c673b43cffbcd31daec80f9f9f4a5c067
g-gar <g.gar.fern@hotmail.com>
2021-11-27 18:37:28 +0100

    Update update.bat

--- update.bat
+++ update.bat
@@ -1,12 +1,12 @@
 @echo off
 
-set base=%CD%
-set venv=%base%\venv
+set basedir=%CD%
+set venv=%basedir%\venv
 set scripts=%venv%\Scripts
 
-call %scripts%\activate.bat
+call "%scripts%"\activate.bat
 python -m pip install --upgrade --force-reinstall wheel setuptools
-python -m pip install --upgrade --force-reinstall -r "%base%"\requirements.txt
-call %scripts%\deactivate.bat
+python -m pip install --upgrade --force-reinstall -r "%basedir%"\requirements.txt
+call "%scripts%"\deactivate.bat
 
 @echo on
