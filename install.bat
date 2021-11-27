7f2518ae35a805404ba871497a0715bb536362ce
g-gar <g.gar.fern@hotmail.com>
2021-11-27 18:36:23 +0100

    Update install.bat

--- install.bat
+++ install.bat
@@ -8,9 +8,9 @@ python -m pip install virtualenv
 
 rmdir /s /q "%venv%"
 python -m venv --upgrade-deps --clear "%venv%"
-call %scripts%\activate.bat
+call "%scripts%"\activate.bat
 python -m pip install wheel setuptools
 python -m pip install --upgrade --force-reinstall -r "%base%"\requirements.txt
-call %scripts%\deactivate.bat
+call "%scripts%"\deactivate.bat
 
 @echo on
